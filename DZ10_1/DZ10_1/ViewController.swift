7176335c85bfb81a795fefac9005bc8cd3dfe840
assasix11 <assasix27@gmail.com>
2021-04-08 18:25:14 +0300

    finish_2

--- DZ10_1/DZ10_1/ViewController.swift
+++ DZ10_1/DZ10_1/ViewController.swift
@@ -21,12 +21,8 @@ class ViewController: UIViewController {
         let u = sender.location(in : touchDetector)
         if x >= 0 && y >= 0 && Double(u.x) > Double(x) && Double(u.x) < Double(x) + 100 && Double(u.y) > Double(y) && Double(u.y) < Double(y) + 90 {
             circle.removeFromSuperview()
-            x = Int.random(in: 0...Int(touchDetector.bounds.size.width) - 100)
-            y = Int.random(in: 100...Int(touchDetector.bounds.size.height) - 90)
-            circle = UILabel(frame: CGRect(x: x, y: y, width: 100, height: 90))
-            circle.text = "🎃"
-            circle.font = .systemFont(ofSize: CGFloat(85))
-            touchDetector.addSubview(circle)
+            x = -1
+            y = -1
         }
         else if x < 0 && y < 0 {
             x = Int.random(in: 0...(Int(touchDetector.bounds.size.width) - 100))
